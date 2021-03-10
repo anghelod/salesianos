f2dd4215b9516a0ad1071de921ab2ab8020614cb
anghelodavid <anghelo780@gmail.com>
2021-03-10 21:44:18 +0100

    cambiar
    se  cambio todo esto

---
+++ ejercicio1.java
@@ -0,0 +1,13 @@
+package TAREAS2;
+
+public class ejercicio1 {
+	public static void main(String[] args) {
+		int i = 0;
+		while(i<10) {
+			System.out.println("Los bucles son muy sencillos");
+			i++;
+		}
+	}
+
+}
+
